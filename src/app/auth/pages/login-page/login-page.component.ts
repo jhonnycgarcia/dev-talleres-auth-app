d61be0ce1c5f787fb6fef2d4a5d518f119123633
Jhonny <jhonnycgarcia@gmail.com>
2024-10-19 01:43:44 -0500

    Post - Login

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   templateUrl: './login-page.component.html',
@@ -8,20 +9,20 @@ import { FormBuilder, Validators } from '@angular/forms';
 export class LoginPageComponent {
 
   private fb = inject(FormBuilder);
+  private authSrv = inject(AuthService);
 
   public myForm = this.fb.group({
-    email: [
-      '',
-      [Validators.required, Validators.email]
-    ],
-    password: [
-      '',
-      [Validators.required, Validators.minLength(6)]
-    ]
+    email: [ '', [ Validators.required, Validators.email ] ],
+    password: [ '', [ Validators.required, Validators.minLength(6) ] ]
   });
 
   login() {
-    console.log('formData', this.myForm.value);
+    const { email, password } = this.myForm.value;
+    if (!email || !password) return;
+    this.authSrv.login(email, password)
+      .subscribe((result) => {
+        console.log('result', result);
+      });
   }
 
 }
