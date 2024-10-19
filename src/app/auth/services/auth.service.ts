a93e290403148424c7ba4117738a8b136903d661
Jhonny <jhonnycgarcia@gmail.com>
2024-10-19 01:31:28 -0500

    Tipos e interfaces

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../../environments/environments';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
+import { AuthStatus, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,8 @@ export class AuthService {
   private readonly baseUrl = environment.baseUrl;
   private http = inject(HttpClient);
 
-  // private _currentUser = signal<any | null>(null);
-  // private _authStatus = signal<AuthStatus>();
+  private _currentUser = signal<User | null>(null);
+  private _authStatus = signal<AuthStatus>(AuthStatus.checking);
 
   login(email: string, password: string): Observable<boolean> {
     // desarrollar
