23f226cd5c09f507fd58371df48e6c8b00f8f979
Jhonny <jhonnycgarcia@gmail.com>
2024-10-19 01:16:33 -0500

    Diseño de la pantalla de Login.- HTML

--- README.md
+++ README.md
@@ -25,3 +25,5 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+
+`https://gist.github.com/Klerith/df3ab2d62b5b20f11fd5db28b06ebb3b`
--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+
+  private fb = inject(FormBuilder);
+
+  public myForm = this.fb.group({
+    email: [
+      '',
+      [Validators.required, Validators.email]
+    ],
+    password: [
+      '',
+      [Validators.required, Validators.minLength(6)]
+    ]
+  });
+
+  login() {
+    console.log('formData', this.myForm.value);
+  }
 
 }
